c62d815ad52598345627201dae167ec1204e0a7d
Julia Bush <j.elizabethbush@gmail.com>
2025-12-05 23:08:47 +0100

    add: using the interfaces .on method to parse the user input and print or handle conditionally

--- src/main.ts
+++ src/main.ts
@@ -9,5 +9,15 @@ main();
 const { createInterface } = require("node:readline");
 const rl = createInterface({
   input: process.stdin,
-  output: process.stdout,
+  output: process.stdout, 
+  prompt: rl.setPrompt(prompt)
 });
+
+rl.on("line", callback) => {
+  if input === [] {
+    rl.prompt()
+  }
+  cleanInput(input)
+  console.log(`Your command was ${prompt}`)
+  rl.prompt()
+}
