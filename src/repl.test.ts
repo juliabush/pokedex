3d2b5ba45c355967784c689628f0f1b20285aa1f
Julia Bush <j.elizabethbush@gmail.com>
2025-12-05 21:46:14 +0100

    add: logic for test to loop over each case

--- src/repl.test.ts
+++ src/repl.test.ts
@@ -6,4 +6,11 @@ describe.each([
     input: "  hello  world  ",
     expected: ["hello", "world"],
   },
-])();
+])("cleanInput($input)", ({ input, expected }) => {
+  test(`Expected: ${expected}`, () => {
+    expect(actual).toHaveLength(expected.length);
+    for (const i in expected) {
+      expect(actual[i]).toBe(expected[i]);
+    }
+  });
+});
