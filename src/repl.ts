ee6f3c7d59a3b74884cb0de660b293608e04f66f
Julia Bush <j.elizabethbush@gmail.com>
2025-12-05 22:12:07 +0100

    add: filling out the cleanInput function to have each word in a list of strings

--- src/repl.ts
+++ src/repl.ts
@@ -1,1 +1,6 @@
-export function cleanInput(input: string): string[] {}
+export function cleanInput(input: string): string[] {
+  let lowercase_string = input.toLowerCase();
+  let trimmed_string = lowercase_string.trim();
+  let splitted_input = trimmed_string.split(/\s+/);
+  return splitted_input;
+}
