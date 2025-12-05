107baf93bd181ca10aa6b8097ddb4951926e28ae
Julia Bush <j.elizabethbush@gmail.com>
2025-12-05 23:11:56 +0100

    remove: moving repl logic for interface into repl.ts

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,3 @@ function main() {
 }
 
 main();
-
-const { createInterface } = require("node:readline");
-const rl = createInterface({
-  input: process.stdin,
-  output: process.stdout, 
-  prompt: rl.setPrompt(prompt)
-});
-
-rl.on("line", callback) => {
-  if input === [] {
-    rl.prompt()
-  }
-  cleanInput(input)
-  console.log(`Your command was ${prompt}`)
-  rl.prompt()
-}
