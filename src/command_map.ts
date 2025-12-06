6d2fda8b241ef3c403fc514b5c02f49265843915
Julia Bush <j.elizabethbush@gmail.com>
2025-12-06 16:34:42 +0100

    add: looping over results in command map function

--- src/command_map.ts
+++ src/command_map.ts
@@ -2,4 +2,9 @@ import type { State } from "./state.js";
 
 export async function commandMap(state: State) {
   const data = await state.pokeApi.fetchLocations(state.nextLocationsURL);
+  for (const data of data.results) {
+    console.log(location.name);
+    state.nextLocationsURL = data.next;
+    state.prevLocationsURL = data.previous;
+  }
 }
