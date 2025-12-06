bcdecc724d24ce21a1e442ed1f14ebfb80704286
Julia Bush <j.elizabethbush@gmail.com>
2025-12-06 12:22:33 +0100

    init & add: creating a command file and defining a couple of command related functions

---
+++ src/command.ts
@@ -0,0 +1,20 @@
+export function getCommands(): Record<string, CLICommand> {
+  return {
+    exit: {
+      name: "exit",
+      description: "Exits the pokedex",
+      callback: commandExit,
+    },
+    help: {
+        name: "help",
+        description: "Provides a help menu"
+        callback: commandHelp,
+    }
+  };
+}
+
+export type CLICommand = {
+  name: string;
+  description: string;
+  callback: (commands: Recod<string, CLICommand>) => void;
+};
