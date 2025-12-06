18d8d131bc75f51450e7062e53e796eb8b90d493
Julia Bush <j.elizabethbush@gmail.com>
2025-12-06 16:30:57 +0100

    add: calling the saync function defined in pokeapi.ts

--- src/command_map.ts
+++ src/command_map.ts
@@ -1,15 +1,5 @@
 import type { State } from "./state.js";
 
 export async function commandMap(state: State) {
-  let pokeApiUrl = "https://pokeapi.co/api/v2/location-area/";
-  try {
-    const response = await fetch(pokeApiUrl);
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    }
-    const result = await response.json();
-    console.log(result);
-  } catch (error) {
-    console.error(error.message);
-  }
+  const data = await state.pokeApi.fetchLocations(state.nextLocationsURL);
 }
