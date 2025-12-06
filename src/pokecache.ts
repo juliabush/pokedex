2e8d551ac427a03923bcf6dae1d8d668db84d68a
Julia Bush <j.elizabethbush@gmail.com>
2025-12-06 18:55:51 +0100

    add: addign two private fields to class cache and defining properties for type Cacheentry

--- src/pokecache.ts
+++ src/pokecache.ts
@@ -1,3 +1,9 @@
 export class Cache {
     #cache = new Map<string>, CacheEntry<any>>();
+    #reapIntervalId: NodeJS.Timeout | undefined = undefined;
+    #interval: number;
+}
+export type CacheEntry<T>(createdAt: number, val<T>): T {
+    createdAt:
+    val: 
 }
