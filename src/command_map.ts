7693003c12c39aa0d375e7955615ccf8874d2dd6
Julia Bush <j.elizabethbush@gmail.com>
2025-12-06 17:27:23 +0100

    update: moving next and previous alterations to before the for loop

--- src/command_map.ts
+++ src/command_map.ts
@@ -2,9 +2,11 @@ import type { State } from "./state.js";
 
 export async function commandMap(state: State): Promise<void> {
   const data = await state.pokeAPI.fetchLocations(state.nextLocationsURL);
+
+  state.nextLocationsURL = data.next;
+  state.prevLocationsURL = data.previous;
+
   for (const location of data.results) {
     console.log(location.name);
-    state.nextLocationsURL = data.next;
-    state.prevLocationsURL = data.previous;
   }
 }
