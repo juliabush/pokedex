25ab2ad82f93b66f89fee99eb1e4a6aca329e200
Julia Bush <j.elizabethbush@gmail.com>
2025-12-06 17:39:01 +0100

    add: creating a conditional to check if your on the first page for map back, if so then return

--- src/command_mapb.ts
+++ src/command_mapb.ts
@@ -1,5 +1,8 @@
 import type { State } from "./state.js";
 
 export async fucntion commandMapBack(state: State): Promise<void> {
-
+    if (!state.prevLocationsURL) {
+        console.log("you're on the first page");
+        return;
+    }
 }
