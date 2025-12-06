4d2c194c00ce53ee0ca590f725feac94a11ef145
Julia Bush <j.elizabethbush@gmail.com>
2025-12-06 12:35:26 +0100

    fix: fixing import file path by adding ./

--- src/command.ts
+++ src/command.ts
@@ -1,5 +1,5 @@
-import { commandExit } from "command_exit";
-import { commandHelp } from "command_help";
+import { commandExit } from "./command_exit";
+import { commandHelp } from "./command_help";
 
 export function getCommands(): Record<string, CLICommand> {
   return {
