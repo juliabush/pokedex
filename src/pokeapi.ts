89e4afcff5cad4d101a16b2ee934aea14decfc4c
Julia Bush <j.elizabethbush@gmail.com>
2025-12-06 16:28:15 +0100

    update: using ur and the class baseURL in fetchLocations

--- src/pokeapi.ts
+++ src/pokeapi.ts
@@ -3,11 +3,28 @@ export class PokeAPI {
 
   constructor() {}
 
-  async fetchLocations(pageURL?: string): Promise<ShallowLocations> {}
+  async fetchLocations(pageURL?: string): Promise<ShallowLocations> {
+    const url = pageURL ?? `${PokeAPI.baseURL}/location-area`;
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
 
   async fetchLocation(locationName: string): Promise<Location> {}
 }
 
-export type ShallowLocations = {};
+export type ShallowLocations = {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: String[];
+};
 
 export type Location = {};
