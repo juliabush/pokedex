466de5edc841557346c9146113c5615c3c4614ac
Julia Bush <j.elizabethbush@gmail.com>
2025-12-06 12:19:15 +0100

    add: filling out the commandExit function

--- src/command_exit.ts
+++ src/command_exit.ts
@@ -2,3 +2,9 @@ export function commandExit() {
   console.log("Closing the Pokedex...\nGoodbye!");
   return process.exit(0);
 }
+
+export type CLICommand = {
+  name: string;
+  description: string;
+  callback: (commands: Recod<string, CLICommand>) => void;
+};
