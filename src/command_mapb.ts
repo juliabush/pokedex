0d5bd79bc04949c0a2f56edd94ea5fb4017f1b53
Julia Bush <j.elizabethbush@gmail.com>
2025-12-06 17:44:48 +0100

    fix: typo function was mispelled

--- src/command_mapb.ts
+++ src/command_mapb.ts
@@ -1,16 +1,16 @@
 import type { State } from "./state.js";
 
-export async fucntion commandMapBack(state: State): Promise<void> {
-    if (!state.prevLocationsURL) {
-        console.log("you're on the first page");
-        return;
-    }
-    const locations = await state.pokeAPI.fetchLocations(state.prevLocationsURL);
+export async function commandMapBack(state: State): Promise<void> {
+  if (!state.prevLocationsURL) {
+    console.log("you're on the first page");
+    return;
+  }
+  const locations = await state.pokeAPI.fetchLocations(state.prevLocationsURL);
 
-    state.nextLocationsURL = locations.next;
-    state.prevocationsURL = locations.previous;
+  state.nextLocationsURL = locations.next;
+  state.prevocationsURL = locations.previous;
 
-    for (const loc of locations.results) {
-        console.log(loc.name)
-    }
+  for (const loc of locations.results) {
+    console.log(loc.name);
+  }
 }
