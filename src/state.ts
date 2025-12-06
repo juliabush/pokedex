bb3e4f74e0cbe26c4bd511a27dc8cbbbafd21745
Julia Bush <j.elizabethbush@gmail.com>
2025-12-06 16:16:35 +0100

    update: adding properties to State making them optional and multi type, also adding them to return statement in function initState()

--- src/state.ts
+++ src/state.ts
@@ -1,15 +1,18 @@
 import { createInterface, type Interface } from "readline";
 import { getCommands } from "./command.js";
+import { PokeAPI } from "./pokeapi.js";
 
 export type CLICommand = {
   name: string;
   description: string;
-  callback: (state: State) => void;
+  callback: (state: State) => Promise<void>;
 };
 
 export type State = {
   readline: Interface;
   commands: Record<string, CLICommand>;
+  nextLocationsURL?: string | null;
+  prevLocationsURL?: string | null;
 };
 
 export function initState() {
@@ -21,5 +24,8 @@ export function initState() {
   return {
     readline: readlineInterface,
     commands: getCommands(),
+    pokeApi: new PokeAPI()
+    nextLocationsURL: undefined,
+    prevLocationsURL: undefined,
   };
 }
