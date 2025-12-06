4fc1fe553a6a0ce55a6642aefe82c8b29b5281a0
Julia Bush <j.elizabethbush@gmail.com>
2025-12-06 12:52:03 +0100

    add: import type CLI command for calbacks added to both files

--- src/command_exit.ts
+++ src/command_exit.ts
@@ -1,10 +1,6 @@
+import type { CLICommand } from "./command";
+
 export function commandExit() {
   console.log("Closing the Pokedex...\nGoodbye!");
   return process.exit(0);
 }
-
-export type CLICommand = {
-  name: string;
-  description: string;
-  callback: (commands: Recod<string, CLICommand>) => void;
-};
--- src/command_help.ts
+++ src/command_help.ts
@@ -1,3 +1,5 @@
+import type { CLICommand } from "./command";
+
 export function commandHelp() {
   console.log("Welcome to the Pokedex!\n Usage:");
 }
