c2ebaca0daf2cecf241c22bebd48931e9f6046a2
Julia Bush <j.elizabethbush@gmail.com>
2025-12-06 13:15:58 +0100

    add: filling out the commandHelp function with looping over commands registry

--- src/command_help.ts
+++ src/command_help.ts
@@ -2,4 +2,7 @@ import type { CLICommand } from "./command.js";
 
 export function commandHelp() {
   console.log("Welcome to the Pokedex!\n Usage:");
+  for (const cmd of Object.values(commands)) {
+    console.log(`${cmd.name}: ${cmd.description}`);
+  }
 }
