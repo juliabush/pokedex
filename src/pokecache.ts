3b653ba1ec37ac15378d7ce9d06576022a20e608
Julia Bush <j.elizabethbush@gmail.com>
2025-12-06 21:32:47 +0100

    add: defining the skeletons for multiple public and private methods

--- src/pokecache.ts
+++ src/pokecache.ts
@@ -2,6 +2,12 @@ export class Cache {
     #cache = new Map<string>, CacheEntry<any>>();
     #reapIntervalId: NodeJS.Timeout | undefined = undefined;
     #interval: number;
+    add<T>() {
+
+    }
+    get<T>() {
+        
+    }
 }
 export type CacheEntry<T> {
     createdAt: number;
