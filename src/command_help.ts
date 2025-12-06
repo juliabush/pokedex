6cd04c6a422c27a3e8cfa39acda2813a83cf37ed
Julia Bush <j.elizabethbush@gmail.com>
2025-12-06 14:20:40 +0100

    update: importing type state from state

--- src/command_help.ts
+++ src/command_help.ts
@@ -1,8 +1,8 @@
-import type { CLICommand } from "./command.js";
+import type { State } from "./state.js";
 
 export function commandHelp() {
   console.log("Welcome to the Pokedex!\n Usage:");
-  for (const cmd of Object.values(commands)) {
+  for (const cmd of state.commands) {
     console.log(`${cmd.name}: ${cmd.description}`);
   }
 }
