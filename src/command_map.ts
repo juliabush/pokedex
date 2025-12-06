3e17716988bfec657bdcac0e49ddbb5bce489ad4
Julia Bush <j.elizabethbush@gmail.com>
2025-12-06 16:54:41 +0100

    fix: chanign from pokeApi to pokeAPI

--- src/command_map.ts
+++ src/command_map.ts
@@ -1,8 +1,8 @@
 import type { State } from "./state.js";
 
 export async function commandMap(state: State) {
-  const data = await state.pokeApi.fetchLocations(state.nextLocationsURL);
-  for (const data of data.results) {
+  const data = await state.pokeAPI.fetchLocations(state.nextLocationsURL);
+  for (const location of data.results) {
     console.log(location.name);
     state.nextLocationsURL = data.next;
     state.prevLocationsURL = data.previous;
