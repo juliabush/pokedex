575f5aac968479f62dba68fe237bef4e694df27c
Julia Bush <j.elizabethbush@gmail.com>
2025-12-06 21:59:03 +0100

    add: roughly filing out the methods for cacheEntry

--- src/pokecache.ts
+++ src/pokecache.ts
@@ -1,15 +1,40 @@
 export class Cache {
-    #cache = new Map<string>, CacheEntry<any>>();
+    #cache = new Map<string, CacheEntry<any>>();
     #reapIntervalId: NodeJS.Timeout | undefined = undefined;
     #interval: number;
-    add<T>() {
-
+    add<T>(key: string, val: T) {
+        return this.#cache.CacheEntry.set(key, val)
     }
-    get<T>() {
+    get<T>(key: string): Object {
+        if (this.#cache.CacheEntry.get(key) === undefined) {
+            return undefined
+        } else {
+            this.#cache.CacheEntry.get(key)
+        }
         
     }
+    #reap() {
+        for entries in Cache{
+            if Cache.cache > Date.now() - this.#interval {
+                    delete
+         } else {
+                
+            }
+        }
+    }
+    #startReapLoop() {
+        setInterval(this.#reap, this.#interval)
+    }
+    stopReapLoop() {
+        clearInterval();
+        this.#reapIntervalId = undefined
+    }
+    constructor(number: number) {
+        number = this.#interval;
+        this.#startReapLoop()
+    }
 }
-export type CacheEntry<T> {
+export type CacheEntry<T> = {
     createdAt: number;
     val: T;
 }
