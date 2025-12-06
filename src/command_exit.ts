a77a0c6f2b4816e224cde120f77f86e7b71e87db
Julia Bush <j.elizabethbush@gmail.com>
2025-12-06 13:12:37 +0100

    Fix: removing newline from commandExit

--- src/command_exit.ts
+++ src/command_exit.ts
@@ -1,6 +1,6 @@
 import type { CLICommand } from "./command.js";
 
 export function commandExit() {
-  console.log("Closing the Pokedex...\nGoodbye!");
+  console.log("Closing the Pokedex... Goodbye!");
   return process.exit(0);
 }
