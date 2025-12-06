0fc0cfea9da9e74fdb8b29b65b5c0f7f9bb8c4ad
Julia Bush <j.elizabethbush@gmail.com>
2025-12-06 23:56:17 +0100

    add: filling out more of test skeleton with logic

--- src/repl.test.ts
+++ src/repl.test.ts
@@ -17,3 +17,27 @@ describe.each([
   });
 });
 // Cache test
+test.concurrent.each({
+  {
+    key: ""
+    val:
+    interval: 500,
+  },
+  {
+    key:
+    val:
+    interval: 1000,
+  }
+})("Test Caching $interval ms", async ({ key,val, interval }) => {
+  const cache = new Cache(interval);
+
+  cache.add(key, val);
+  const cached = cache.get(key);
+  expect(cached).toBe(val);
+
+  await new Promise((resolve) => setTimeout(resolve, interval + 100));
+  const reaped = cache.get(key);
+  expect(reaped).toBe(undefined);
+
+  cache.stopReapLoop()
+})
