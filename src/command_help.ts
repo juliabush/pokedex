74090f9ad7f9b241530e6a5d58235b20828eac26
Julia Bush <j.elizabethbush@gmail.com>
2025-12-06 16:46:11 +0100

    update: making the command help async

--- src/command_help.ts
+++ src/command_help.ts
@@ -1,6 +1,6 @@
 import type { State } from "./state.js";
 
-export function commandHelp(state: State) {
+export async function commandHelp(state: State) {
   console.log("Welcome to the Pokedex!\n Usage:");
   for (const cmd of Object.values(state.commands)) {
     console.log(`${cmd.name}: ${cmd.description}`);
