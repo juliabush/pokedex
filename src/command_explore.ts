c54f84911a32354d3f8635341f033337254210fc
Julia Bush <j.elizabethbush@gmail.com>
2025-12-07 14:38:04 +0100

    add: temporrary debugging line

--- src/command_explore.ts
+++ src/command_explore.ts
@@ -4,6 +4,7 @@ export async function commandExplore(
   state: State,
   locationName: string
 ): Promise<void> {
+  console.log(`DEBUG: locationName = ${locationName}`);
   const locations = await state.pokeAPI.fetchLocation(locationName);
   let result = locations.pokemon_encounters;
   console.log(`Exploring ${locationName}...`);
