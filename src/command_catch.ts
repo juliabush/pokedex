c8a7af6e0efb2d291636b5aa7696fe152562a9dc
Julia Bush <j.elizabethbush@gmail.com>
2025-12-07 15:18:38 +0100

    add: building out the catch command for pokemon

--- src/command_catch.ts
+++ src/command_catch.ts
@@ -1,1 +1,11 @@
 import { State } from "state.js";
+
+export async function commandCatch(
+  state: State,
+  pokemonName: string
+): Promise<void> {
+  const pokemon = await state.pokeAPI.fetchPokemon(pokemonName);
+  let result = pokemon.name;
+  let difficulty = pokemon.base_experience;
+  console.log(`Throwing a Pokeball at ${pokemonName}...`);
+}
