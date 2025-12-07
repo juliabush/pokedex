1adf600d86c3444cd2c5256a98697eba542498f5
Julia Bush <j.elizabethbush@gmail.com>
2025-12-07 15:32:56 +0100

    add: switch statement for when pokemon is caught or escapes

--- src/command_catch.ts
+++ src/command_catch.ts
@@ -8,4 +8,26 @@ export async function commandCatch(
   let result = pokemon.name;
   let difficulty = pokemon.base_experience;
   console.log(`Throwing a Pokeball at ${pokemonName}...`);
+  switch (difficulty):
+    case1 (difficult > 250):
+    let caught = (Math.round() * 10)
+    if (caught === 3) {
+        console.log(`${pokemonName} was caught!`)
+    } else {
+        console.log(`${pokemonName} escaped!`)
+    }
+    case2 (difficulty > 100):
+     let caught = (Math.round() * 5)
+    if (caught === 3) {
+        console.log(`${pokemonName} was caught!`)
+    } else {
+        console.log(`${pokemonName} escaped!`)
+    }
+     case2 (difficulty > 0):
+     let caught = (Math.round() * 2)
+    if (caught === 2) {
+        console.log(`${pokemonName} was caught!`)
+    } else {
+        console.log(`${pokemonName} escaped!`)
+    }
 }
