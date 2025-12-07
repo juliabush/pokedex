9db57b4ee8f2ee7bb02450ff81cdff4766bc12cf
Julia Bush <j.elizabethbush@gmail.com>
2025-12-07 15:45:04 +0100

    add: defining the catch command in the get commands file

--- src/command.ts
+++ src/command.ts
@@ -3,6 +3,7 @@ import { commandHelp } from "./command_help.js";
 import { commandMap } from "./command_map.js";
 import { commandMapBack } from "./command_mapb.js";
 import { commandExplore } from "./command_explore.js";
+import { commandCatch } from "./command_catch.js";
 import { CLICommand } from "./state.js";
 
 export function getCommands(): Record<string, CLICommand> {
@@ -31,6 +32,11 @@ export function getCommands(): Record<string, CLICommand> {
       name: "explore",
       description: "Exploring diffrent locations",
       callback: commandExplore,
+    },
+    catch: {
+      name: "catch",
+      description: "Catch different pokemon",
+      callback: commandCatch,
     },
   };
 }
