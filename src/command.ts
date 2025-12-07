deb039d5bb0db9e4590fd17cef2a951cc663aad7
Julia Bush <j.elizabethbush@gmail.com>
2025-12-07 16:20:11 +0100

    add: import of command inspect to command registry'

--- src/command.ts
+++ src/command.ts
@@ -4,6 +4,7 @@ import { commandMap } from "./command_map.js";
 import { commandMapBack } from "./command_mapb.js";
 import { commandExplore } from "./command_explore.js";
 import { commandCatch } from "./command_catch.js";
+import { commandInspect } from "./command_inspect.js";
 import { CLICommand } from "./state.js";
 
 export function getCommands(): Record<string, CLICommand> {
