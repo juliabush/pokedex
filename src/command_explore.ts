d5cf9b02a09b07a050dcd4ffdd22c139ca19c6a5
Julia Bush <j.elizabethbush@gmail.com>
2025-12-07 13:48:55 +0100

    add: filling out the command explore to fetch from PokeAPI just like other commands

--- src/command_explore.ts
+++ src/command_explore.ts
@@ -1,4 +1,12 @@
 export async function commandExplore(
   state: State,
   locationArea: string
-): Promise<void> {}
+): Promise<void> {
+  const locations = await state.pokeAPI.fetchLocations(state.nextLocationsURL);
+  state.nextLocationsURL = locations;
+  state.prevLocationsURL = locations.previous;
+
+  for (const loc of locations.results) {
+    console.log(loc.name);
+  }
+}
