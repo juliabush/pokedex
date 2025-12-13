044e2b76c0e8e077d2df0449ea3fe138803d2fbf
Julia Bush <j.elizabethbush@gmail.com>
2025-12-13 15:13:26 +0100

    update: modifying command map to be compatible with frontend

--- backend/src/command_map.ts
+++ backend/src/command_map.ts
@@ -1,6 +1,8 @@
 import type { State } from "./state.js";
 
-export async function commandMap(state: State): Promise<void> {
+export async function commandMap(
+  state: State
+): Promise<{ locations: string[] }> {
   const locations = await state.pokeAPI.fetchLocations(
     state.nextLocationsURL ?? undefined
   );
@@ -8,7 +10,7 @@ export async function commandMap(state: State): Promise<void> {
   state.nextLocationsURL = locations.next;
   state.prevLocationsURL = locations.previous;
 
-  for (const loc of locations.results) {
-    console.log(loc.name);
-  }
+  return {
+    locations: locations.results.map((loc) => loc.name),
+  };
 }
