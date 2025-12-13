0b2f2a01f183b015cefa4752a6354698b6e103c8
Julia Bush <j.elizabethbush@gmail.com>
2025-12-13 20:30:54 +0100

    add: pokeball scene to Home page

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,9 +1,10 @@
 import "./Home.css";
+import PokeballScene from "./PokeballScene";
 
 export default function Home() {
   return (
     <div className="home">
-      <h1>Home</h1>
+      <PokeballScene />
     </div>
   );
 }
