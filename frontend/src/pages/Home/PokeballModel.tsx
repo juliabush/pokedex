46a3f05b9cbd0a63f37f98dc2c53adf888b95f0f
Julia Bush <j.elizabethbush@gmail.com>
2025-12-13 21:44:02 +0100

    update: refactor PokeballModel to accept refs for animation

--- frontend/src/pages/Home/PokeballModel.tsx
+++ frontend/src/pages/Home/PokeballModel.tsx
@@ -1,26 +1,41 @@
-export default function PokeballModel() {
-  return (
-    <group>
-      {/* Red half of Pokeball */}
-      <mesh rotation={[Math.PI, 0, 0]} scale={[1.02, 1.02, 1.02]}>
-        <sphereGeometry args={[5, 32, 32, 0, Math.PI * 2, 0, Math.PI / 2]} />
-        <meshStandardMaterial color="white" />
-      </mesh>
-      {/* White half of Pokeball */}
-      <mesh scale={[1.02, 1.02, 1.02]}>
-        <sphereGeometry args={[5, 32, 32, 0, Math.PI * 2, 0, Math.PI / 2]} />
-        <meshStandardMaterial color="red" />
-      </mesh>
-      {/* Black band around the center of Pokeball */}
-      <mesh scale={[1.02, 1.02, 1.02]}>
-        <cylinderGeometry args={[5.05, 5.05, 1.4, 64]} />
-        <meshStandardMaterial color="black" />
-      </mesh>
-      {/* Center button of Pokeball */}
-      <mesh position={[0, 0, 5.25]} rotation={[Math.PI / 2, 0, 0]}>
-        <cylinderGeometry args={[0.6, 0.6, 0.05, 64]} />
-        <meshStandardMaterial color="white" />
-      </mesh>
-    </group>
-  );
-}
+import { forawrdRef } from "react";
+import * as THREE from "three";
+
+type Props = {
+  topRef: React.RefObject<THREE.Mesh>;
+};
+
+const PokeballModel = forwardRef<THREE.UniformsGroup, Props>(
+  ({ topRef }, groupRef) => {
+    return (
+      <group ref={groupRef}>
+        {/* Red half of Pokeball */}
+        <mesh
+          ref={topRef}
+          rotation={[Math.PI, 0, 0]}
+          scale={[1.02, 1.02, 1.02]}
+        >
+          <sphereGeometry args={[5, 32, 32, 0, Math.PI * 2, 0, Math.PI / 2]} />
+          <meshStandardMaterial color="white" />
+        </mesh>
+        {/* White half of Pokeball */}
+        <mesh scale={[1.02, 1.02, 1.02]}>
+          <sphereGeometry args={[5, 32, 32, 0, Math.PI * 2, 0, Math.PI / 2]} />
+          <meshStandardMaterial color="red" />
+        </mesh>
+        {/* Black band around the center of Pokeball */}
+        <mesh scale={[1.02, 1.02, 1.02]}>
+          <cylinderGeometry args={[5.05, 5.05, 1.4, 64]} />
+          <meshStandardMaterial color="black" />
+        </mesh>
+        {/* Center button of Pokeball */}
+        <mesh position={[0, 0, 5.25]} rotation={[Math.PI / 2, 0, 0]}>
+          <cylinderGeometry args={[0.6, 0.6, 0.05, 64]} />
+          <meshStandardMaterial color="white" />
+        </mesh>
+      </group>
+    );
+  }
+);
+
+export default PokeballModel;
