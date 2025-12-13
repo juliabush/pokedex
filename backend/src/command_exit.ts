f6f0c1255856e7f4d8e902b4b3c354cc33462d9a
Julia Bush <j.elizabethbush@gmail.com>
2025-12-13 14:24:36 +0100

    update: making the command exit frontend compatible; this command may become useless

--- backend/src/command_exit.ts
+++ backend/src/command_exit.ts
@@ -1,8 +1,5 @@
 import type { State } from "./state.js";
 
-export async function commandExit(state: State): Promise<void> {
-  const rl = state.readline;
-  console.log("Closing the Pokedex... Goodbye!");
-  rl.close();
-  return process.exit(0);
+export function commandExit(state: State): { exit: true } {
+  return { exit: true };
 }
