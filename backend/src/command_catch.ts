728febbcd9377ccd9e5212f689113bd36b97d135
Julia Bush <j.elizabethbush@gmail.com>
2025-12-13 14:19:11 +0100

    update: modifying the catch command to return values as opposed to print to terminal

--- backend/src/command_catch.ts
+++ backend/src/command_catch.ts
@@ -3,30 +3,25 @@ import type { State } from "./state.js";
 export async function commandCatch(
   state: State,
   pokemonName: string
-): Promise<void> {
+): Promise<{ pokemon: string; caught: boolean }> {
   const pokemon = await state.pokeAPI.fetchPokemon(pokemonName);
-  let result = pokemon.name;
   let difficulty = pokemon.base_experience;
-
-  console.log(`Throwing a Pokeball at ${pokemonName}...`);
 
   let caught = false;
 
   if (difficulty > 250) {
-    const roll = Math.floor(Math.random() * 10);
-    caught = roll === 3;
+    caught = Math.floor(Math.random() * 10) === 3;
   } else if (difficulty > 100) {
-    const roll = Math.floor(Math.random() * 5);
-    caught = roll === 3;
+    caught = Math.floor(Math.random() * 5) === 3;
   } else {
-    const roll = Math.floor(Math.random() * 2);
-    caught = roll === 1;
+    caught = Math.floor(Math.random() * 2) === 1;
   }
 
   if (caught) {
-    console.log(`${pokemonName} was caught!`);
     state.pokedex[pokemonName] = pokemon;
-  } else {
-    console.log(`${pokemonName} escaped!`);
   }
+  return {
+    pokemon: pokemon.name,
+    caught,
+  };
 }
