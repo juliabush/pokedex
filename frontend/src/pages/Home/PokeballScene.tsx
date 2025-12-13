08b43d943a315e5c4067923c5d47e949fcea4131
Julia Bush <j.elizabethbush@gmail.com>
2025-12-13 21:19:23 +0100

    update: fixing typo and swapping around rendered components

--- frontend/src/pages/Home/PokeballScene.tsx
+++ frontend/src/pages/Home/PokeballScene.tsx
@@ -31,9 +31,8 @@ function Pokeball() {
   });
 
   return (
-    <group ref={mesh} onClick={() => setShaking(true)}>
-      <sphereGeometry args={[5, 32, 32]} />
-      <meshStandardMaterial color={0xff6347} />
+    <group ref={group} onClick={() => setShaking(true)}>
+      <PokeballModel />
     </group>
   );
 }
@@ -43,7 +42,7 @@ export default function PokeballScene() {
     <Canvas camera={{ position: [0, 0, 20], fov: 75 }}>
       <ambientLight intensity={0.5} />
       <pointLight position={[5, 5, 5]} />
-      <PokeballModel />
+      <Pokeball />
     </Canvas>
   );
 }
