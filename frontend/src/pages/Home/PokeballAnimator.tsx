6a40260005e67739feb5674f415c77ab52cbe642
Julia Bush <j.elizabethbush@gmail.com>
2025-12-13 22:18:39 +0100

    add: useEffect shaking

--- frontend/src/pages/Home/PokeballAnimator.tsx
+++ frontend/src/pages/Home/PokeballAnimator.tsx
@@ -4,3 +4,20 @@ import * as THREE from "three";
 import PokeballModel from "./PokeballModel";
 
 type Phase = "idle" | "shaking" | "opening";
+
+export default function PokeballAnimator() {
+  const groupRef = useef<THREE.Group | null>(null);
+  const topRef = useRef<THREE.Mesh | null>(null);
+
+  const [phase, setPhase] = useState<Phase>("idle");
+
+  useEffect(() => {
+    if (phase !== "shaking") return;
+
+    const t = setTimeout(() => {
+      setPhase("opening");
+    }, 1500);
+
+    return () => clearTimeout(t);
+  }, [phase]);
+}
