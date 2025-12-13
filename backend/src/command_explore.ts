1f2f546717817a47c9e26ade1b15e1b844ce37bb
Julia Bush <j.elizabethbush@gmail.com>
2025-12-13 14:31:03 +0100

    update: making commandExplore frontend compatible

--- backend/src/command_explore.ts
+++ backend/src/command_explore.ts
@@ -3,12 +3,15 @@ import { State } from "./state.js";
 export async function commandExplore(
   state: State,
   locationName: string
-): Promise<void> {
+): Promise<{ location: string; pokemon: string[] }> {
   const locations = await state.pokeAPI.fetchLocation(locationName);
-  let result = locations.pokemon_encounters;
-  console.log(`Exploring ${locationName}...`);
-  console.log(`Found Pokemon:`);
-  for (const pokes of result) {
-    console.log(`- ${pokes.pokemon.name}`);
-  }
+
+  const pokemon = location.pokemon_encounters.map(
+    (encounter) => encounter.pokemon.name
+  );
+
+  return {
+    location: locationName,
+    pokemon,
+  };
 }
