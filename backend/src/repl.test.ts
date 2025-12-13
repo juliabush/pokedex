4d323794d95999c5a5a263da49c16b6f6162722f
Julia Bush <j.elizabethbush@gmail.com>
2025-12-13 16:58:58 +0100

    update: removing the clean input test since we arent using the readline anymore

--- backend/src/repl.test.ts
+++ backend/src/repl.test.ts
@@ -1,22 +1,6 @@
-import { cleanInput } from "./repl";
 import { Cache } from "./pokecache.js";
 import { describe, expect, test } from "vitest";
 
-// cleanInput test
-describe.each([
-  {
-    input: "  hello  world  ",
-    expected: ["hello", "world"],
-  },
-])("cleanInput($input)", ({ input, expected }) => {
-  test(`Expected: ${expected}`, () => {
-    let actual = cleanInput(input);
-    expect(actual).toHaveLength(expected.length);
-    for (const i in expected) {
-      expect(actual[i]).toBe(expected[i]);
-    }
-  });
-});
 // Cache test
 test.concurrent.each([
   {
