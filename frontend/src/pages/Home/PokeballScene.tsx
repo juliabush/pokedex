bc08fd631b2e41e0977f51ded91c2acb7103960a
Julia Bush <j.elizabethbush@gmail.com>
2025-12-14 00:19:41 +0100

    add: adjusting lighting and adding an enviorment to  make the pokeball bright and more 3d

--- frontend/src/pages/Home/PokeballScene.tsx
+++ frontend/src/pages/Home/PokeballScene.tsx
@@ -1,11 +1,14 @@
 import { Canvas } from "@react-three/fiber";
 import PokeballAnimator from "./PokeballAnimator";
+import { Environment } from "@react-three/drei";
 
 export default function PokeballScene() {
   return (
-    <Canvas camera={{ position: [0, 0, 20], fov: 75 }}>
-      <ambientLight intensity={0.5} />
-      <pointLight position={[5, 5, 5]} />
+    <Canvas camera={{ position: [2, 1.5, 20], fov: 75 }}>
+      <ambientLight intensity={0.2} />
+      <pointLight position={[6, 6, 10]} intensity={1.2} />
+      <Environment preset="studio" />
+
       <PokeballAnimator />
     </Canvas>
   );
