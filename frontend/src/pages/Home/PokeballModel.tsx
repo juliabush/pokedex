fbe3d9b6c173c58d3e504de913915ee05a384627
Julia Bush <j.elizabethbush@gmail.com>
2025-12-13 21:30:36 +0100

    fix: bringing circular button forward for design

--- frontend/src/pages/Home/PokeballModel.tsx
+++ frontend/src/pages/Home/PokeballModel.tsx
@@ -1,24 +1,24 @@
 export default function PokeballModel() {
   return (
     <group>
-      {/* Red top half of Pokeball */}
-      <mesh rotation={[Math.PI, 0, 0]}>
-        <sphereGeometry args={[5, 32, 32, 0, Math.PI * 2, 0, Math.PI / 2]} />
-        <meshStandardMaterial color="red" />
-      </mesh>
-      {/* Bottom white half of Pokeball */}
-      <mesh>
+      {/* Red half of Pokeball */}
+      <mesh rotation={[Math.PI, 0, 0]} scale={[1.02, 1.02, 1.02]}>
         <sphereGeometry args={[5, 32, 32, 0, Math.PI * 2, 0, Math.PI / 2]} />
         <meshStandardMaterial color="white" />
       </mesh>
+      {/* White half of Pokeball */}
+      <mesh scale={[1.02, 1.02, 1.02]}>
+        <sphereGeometry args={[5, 32, 32, 0, Math.PI * 2, 0, Math.PI / 2]} />
+        <meshStandardMaterial color="red" />
+      </mesh>
       {/* Black band around the center of Pokeball */}
-      <mesh>
-        <cylinderGeometry args={[5.05, 5.05, 0.4, 64]} />
+      <mesh scale={[1.02, 1.02, 1.02]}>
+        <cylinderGeometry args={[5.05, 5.05, 1.4, 64]} />
         <meshStandardMaterial color="black" />
       </mesh>
       {/* Center button of Pokeball */}
-      <mesh position={[0, 0, 5]}>
-        <cylinderGeometry args={[0.6, 0.6, 0.2, 32]} />
+      <mesh position={[0, 0, 5.25]} rotation={[Math.PI / 2, 0, 0]}>
+        <cylinderGeometry args={[0.6, 0.6, 0.05, 64]} />
         <meshStandardMaterial color="white" />
       </mesh>
     </group>
