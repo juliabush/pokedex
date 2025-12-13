6b4c48c521051d3bcefdad45628533bb9e1cb64c
Julia Bush <j.elizabethbush@gmail.com>
2025-12-13 18:05:01 +0100

    update: defining a return on commandInspect and changes so that it works with frontend

--- backend/src/command_inspect.ts
+++ backend/src/command_inspect.ts
@@ -14,25 +14,21 @@ export function commandInspect(
       types: string[];
     }
   | { found: false } {
-  let pokemon = state.pokedex[pokemonName];
-  if (pokemon !== undefined && pokemon !== null) {
-    let name = pokemon.name;
-    let height = pokemon.height;
-    let weight = pokemon.weight;
-    let stats = pokemon.stats;
-    let types = pokemon.types;
-    console.log(
-      `\nName: ${pokemon.name}\nHeight: ${pokemon.height}\nWeight: ${pokemon.weight}\n`
-    );
-    console.log(`\nStats:\n`);
-    for (const individualStat of pokemon.stats) {
-      console.log(`-${individualStat.stat.name}: ${individualStat.base_stat}`);
-    }
-    console.log(`\nType:\n`);
-    for (const individualType of pokemon.types) {
-      console.log(`-${individualType.type.name}`);
-    }
-  } else {
-    console.log("you have not caught that pokemon");
+  const pokemon = state.pokedex[pokemonName];
+
+  if (!pokemon) {
+    return { found: false };
   }
+
+  return {
+    found: true,
+    name: pokemon.name,
+    height: pokemon.height,
+    weight: pokemon.weight,
+    stats: pokemon.stats.map((s) => ({
+      name: s.stat.name,
+      value: s.base_stat,
+    })),
+    types: pokemon.types.map((t) => t.type.name),
+  };
 }
