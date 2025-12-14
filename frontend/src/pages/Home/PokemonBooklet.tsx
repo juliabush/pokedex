4bc438cdd9cb599337f494ffd03e68cdb75f42e8
Julia Bush <j.elizabethbush@gmail.com>
2025-12-14 22:59:18 +0100

    add: red x on pokemon that has been caught previously

--- frontend/src/pages/Home/PokemonBooklet.tsx
+++ frontend/src/pages/Home/PokemonBooklet.tsx
@@ -5,21 +5,36 @@ type Props = {
   pokemon: Pokemon[];
   selected: string;
   onSelect: (id: string) => void;
+  disabled: Set<string>;
 };
 
-export default function PokemonBooklet({ pokemon, selected, onSelect }: Props) {
+export default function PokemonBooklet({
+  pokemon,
+  selected,
+  onSelect,
+  disabled,
+}: Props) {
   return (
     <div className="booklet">
-      {pokemon.map((p) => (
-        <button
-          key={p.id}
-          className={p.id === selected ? "active" : ""}
-          onClick={() => onSelect(p.id)}
-        >
-          <img src={p.icon} />
-          <span>{p.name}</span>
-        </button>
-      ))}
+      {pokemon.map((p) => {
+        const isDisabled = disabled.has(p.id);
+
+        return (
+          <button
+            key={p.id}
+            className={[
+              p.id === selected ? "active" : "",
+              isDisabled ? "disabled" : "",
+            ].join(" ")}
+            disabled={isDisabled}
+            onClick={() => onSelect(p.id)}
+          >
+            <img src={p.icon} />
+            <span>{p.name}</span>
+            {isDisabled && <span className="x">✕</span>}
+          </button>
+        );
+      })}
     </div>
   );
 }
