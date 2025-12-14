241991b5ecdad59716ad375d190a27f437942ac0
Julia Bush <j.elizabethbush@gmail.com>
2025-12-14 14:13:22 +0100

    remove & fix: using built in sky from react three, all cloud images looked flat

--- frontend/src/pages/Home/PokeballScene.tsx
+++ frontend/src/pages/Home/PokeballScene.tsx
@@ -1,21 +1,22 @@
 import { Canvas } from "@react-three/fiber";
 import PokeballAnimator from "./PokeballAnimator";
-import { Environment } from "@react-three/drei";
-import { useLoader } from "@react-three/fiber";
-import * as THREE from "three";
-
-function Background() {
-  const texture = useLoader(THREE.TextureLoader, "/blue.jpg");
-  return <primitive object={texture} attach="background" />;
-}
+import { Environment, Sky } from "@react-three/drei";
 
 export default function PokeballScene() {
   return (
     <Canvas camera={{ position: [2, 1.5, 20], fov: 75 }}>
-      <Background />
+      <Sky
+        sunPosition={[100, 20, 100]}
+        turbidity={2}
+        rayleigh={5}
+        mieCoefficient={0.003}
+        mieDirectionalG={0.7}
+      />
+
       <ambientLight intensity={0.2} />
       <pointLight position={[6, 6, 10]} intensity={1.2} />
       <Environment preset="studio" />
+
       <PokeballAnimator />
     </Canvas>
   );
