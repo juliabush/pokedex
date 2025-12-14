e0a1a599530419b1bfa91dd65ce4e6b1155f0e36
Julia Bush <j.elizabethbush@gmail.com>
2025-12-14 23:01:03 +0100

    add: reset button that brings pokeball back to idle

--- frontend/src/pages/Home/ResetButton.tsx
+++ frontend/src/pages/Home/ResetButton.tsx
@@ -1,3 +1,13 @@
+import "./ResetButton.css";
+
 type Props = {
   onReset: () => void;
 };
+
+export default function ResetButton({ onReset }: Props) {
+  return (
+    <button className="reset-button" onClick={onReset}>
+      Reset
+    </button>
+  );
+}
