ee6de0a95bc9a627955cdf9cc614cf9265ec2bcc
Julia Bush <j.elizabethbush@gmail.com>
2025-12-14 15:03:28 +0100

    add: pokemon selection state and booklet UI to home

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import "./Home.css";
 import PokeballScene from "./PokeballScene";
+import PokemonBooklet from "./PokemonBooklet";
+import { POKEMON } from "../../data/pokemon";
 
 export default function Home() {
+  const [selectedPokemon, setSelectedPokemon] = useState("pikachu");
+
   return (
     <div className="home">
-      <PokeballScene />
+      <PokeballScene selectedPokemon={selectedPokemon} />
+
+      <PokemonBooklet
+        pokemon={POKEMON}
+        selected={selectedPokemon}
+        onSelect={setSelectedPokemon}
+      />
     </div>
   );
 }
