55cb2962cf2a97ff419ca466d7930861c4306c15
Julia Bush <j.elizabethbush@gmail.com>
2025-12-14 14:43:11 +0100

    add: pokemon booklet allowing users to select pokemon they want to catch

--- frontend/src/pages/Home/PokemonBooklet.tsx
+++ frontend/src/pages/Home/PokemonBooklet.tsx
@@ -9,3 +9,20 @@ type Props = {
   selected: string;
   onSelect: (id: string) => void;
 };
+
+export default function PokemonBooklet({ pokemon, selected, onSelect }: Props) {
+  return (
+    <div className="booklet">
+      {pokemon.map((p) => (
+        <button
+          key={p.id}
+          className={p.id === selected ? "active" : ""}
+          onClick={() => onSelect(p.id)}
+        >
+          <img src={p.icon} />
+          <span>{p.name}</span>
+        </button>
+      ))}
+    </div>
+  );
+}
