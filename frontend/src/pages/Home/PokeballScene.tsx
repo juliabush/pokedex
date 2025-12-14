d9417f4ddf399ce4fc8138b2a24205a0d8463ccb
Julia Bush <j.elizabethbush@gmail.com>
2025-12-14 13:57:30 +0100

    feat: changing the background to look like a sky

--- frontend/src/pages/Home/PokeballScene.tsx
+++ frontend/src/pages/Home/PokeballScene.tsx
@@ -1,14 +1,21 @@
 import { Canvas } from "@react-three/fiber";
 import PokeballAnimator from "./PokeballAnimator";
 import { Environment } from "@react-three/drei";
+import { useLoader } from "@react-three/fiber";
+import * as THREE from "three";
+
+function Background() {
+  const texture = useLoader(THREE.TextureLoader, "/blue.jpg");
+  return <primitive object={texture} attach="background" />;
+}
 
 export default function PokeballScene() {
   return (
     <Canvas camera={{ position: [2, 1.5, 20], fov: 75 }}>
+      <Background />
       <ambientLight intensity={0.2} />
       <pointLight position={[6, 6, 10]} intensity={1.2} />
       <Environment preset="studio" />
-
       <PokeballAnimator />
     </Canvas>
   );
