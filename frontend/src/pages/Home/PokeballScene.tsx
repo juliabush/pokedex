d63b8475cf31f2789e35eede5941e9d0618a124e
Julia Bush <j.elizabethbush@gmail.com>
2025-12-14 22:57:10 +0100

    add: accepting oncaught and passing it forward

--- frontend/src/pages/Home/PokeballScene.tsx
+++ frontend/src/pages/Home/PokeballScene.tsx
@@ -1,11 +1,14 @@
 import { Canvas } from "@react-three/fiber";
 import PokeballAnimator from "./PokeballAnimator";
 import { Environment, Sky } from "@react-three/drei";
+import type { PokemonInspect } from "../../types/pokemon";
 
 export default function PokeballScene({
   selectedPokemon,
+  onCaught,
 }: {
   selectedPokemon: string;
+  onCaught: (pokemon: PokemonInspect) => void;
 }) {
   return (
     <Canvas camera={{ position: [0, 0, 20], fov: 75 }}>
@@ -21,7 +24,7 @@ export default function PokeballScene({
       <pointLight position={[6, 6, 10]} intensity={1.2} />
       <Environment preset="studio" />
 
-      <PokeballAnimator selectedPokemon={selectedPokemon} />
+      <PokeballAnimator selectedPokemon={selectedPokemon} onCaught={onCaught} />
     </Canvas>
   );
 }
