82556044282a9a1da2fa35c8ba4262e4b72738b2
Julia Bush <j.elizabethbush@gmail.com>
2025-12-14 14:27:49 +0100

    feat: add selectable Pokemon list data

---
+++ frontend/src/data/pokemon.ts
@@ -0,0 +1,14 @@
+export const POKEMON = [
+  { id: "pikachu", name: "Pikachu", icon: "/icons/pikachu.png" },
+  { id: "charizard", name: "Charizard", icon: "/icons/charizard.png" },
+  { id: "bulbasaur", name: "Bulbasaur", icon: "/icons/bulbasaur.png" },
+  { id: "squirtle", name: "Squirtle", icon: "/icons/squirtle.png" },
+  { id: "mewtwo", name: "Mewtwo", icon: "/icons/mewtwo.png" },
+  { id: "eevee", name: "Eevee", icon: "/icons/eevee.png" },
+  { id: "snorlax", name: "Snorlax", icon: "/icons/snorlax.png" },
+  { id: "gengar", name: "Gengar", icon: "/icons/gengar.png" },
+  { id: "lucario", name: "Lucario", icon: "/icons/lucario.png" },
+  { id: "jigglypuff", name: "Jigglypuff", icon: "/icons/jigglypuff.png" },
+  { id: "greninja", name: "Greninja", icon: "/icons/greninja.png" },
+  { id: "rayquaza", name: "Rayquaza", icon: "/icons/rayquaza.png" },
+] as const;
