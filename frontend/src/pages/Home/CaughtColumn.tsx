e5622e6875d90d00260594a071597dae190f5775
Julia Bush <j.elizabethbush@gmail.com>
2025-12-14 22:44:57 +0100

    add: defining a CaughtColumn function

--- frontend/src/pages/Home/CaughtColumn.tsx
+++ frontend/src/pages/Home/CaughtColumn.tsx
@@ -1,3 +1,18 @@
+import type { PokemonInspect } from "../../types/pokemon";
+import "./CaughtColumn.css";
+
 type Props = {
   caught: PokemonInspect[];
 };
+
+export default function CaughtColumn({ caught }: Props) {
+  return (
+    <div className="caught-column">
+      {caught.slice(0, 3).map((pokemon, index) => (
+        <div key={pokemon.name} className="caught-card enter">
+          <div className="caught-card-name">{pokemon.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
